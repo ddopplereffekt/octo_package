88ba8ae58ad709fc18b9b74876df93c87474575c
Ddopplereffekt <philip.hirschle@stud.tu-darmstadt.de>
2024-07-01 09:10:34 +0200

    working installation setup

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name="octo", packages=["octo"])
+# setup(name="octo", packages=["octo"])
+setup(
+    name="octo",
+    packages=find_packages(include=["octo", "octo.*"]),
+)
